a611322c1d2fe4254a8ddd0d083df3ab42e2225d
Bruno Heidelberger <bruno.heidelberger@finambo.com>
2013-02-25 16:06:39 +0100

    Make puzzle configurable

--- js/sudokulino.js
+++ js/sudokulino.js
@@ -121,7 +121,42 @@ Puzzle.prototype.validate = function() {
 }
 
 function SudokuCtrl($scope) {
-  $scope.dimension = 9;
-  $scope.puzzle = new Puzzle($scope.dimension);
+  $scope.initialize = function() {
+    $scope.puzzle = new Puzzle($scope.dimension);
+    $scope.rescale($scope.scale);
+  };
+
+  $scope.rescale = function() {
+    var boxDimension = Math.sqrt($scope.dimension);
+
+    var scale = $scope.scale;
+    $scope.cellStyle = { 'width': scale + "px", 'height': scale + "px", 'font-size': scale * 0.6 + "px" };
+
+    scale = (scale + 2) * boxDimension;
+    $scope.boxStyle = { 'width': scale + "px", 'height': scale + "px" };
+
+    scale = (scale + 2) * boxDimension;
+    $scope.puzzleStyle = { 'width': scale + "px", 'height': scale + "px" };
+  };
+
+  (function() {
+    $scope.scales = [
+      { name: 'small', value: 20 },
+      { name: 'medium', value: 30 },
+      { name: 'large', value: 40 }
+    ];
+    $scope.scale = $scope.scales[1].value;
+
+    $scope.dimensions = [
+      { name: '1x1', value: 1 },
+      { name: '2x2', value: 4 },
+      { name: '3x3', value: 9 },
+      { name: '4x4', value: 16 },
+      { name: '5x5', value: 25 }
+    ];
+    $scope.dimension = $scope.dimensions[2].value;
+
+    $scope.initialize();
+  })();
 };
 
